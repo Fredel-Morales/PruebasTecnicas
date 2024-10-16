4b6ed242a3008e2c85cf4494bf31116bcbb77c73
Fredel-Morales <moralesfredel@gmail.com>
2024-10-15 23:06:24 -0600

    Add files via upload

---
+++ PT6/main.py
@@ -0,0 +1,12 @@
+
+#---------- Fredel Morales Jimenez ------------------
+#------- RETO 6: PREGUNTA DE REFLEXION ---------------
+
+# Para abrir el archivo en modo lectura
+with open('reto_seis_ok.txt', 'r', encoding='utf-8') as archivo:
+    
+    # Lee todo el contenido del archivo
+    contenido = archivo.read()  
+
+# ----- Imprime el contenido del archivo -------
+print(contenido)  
